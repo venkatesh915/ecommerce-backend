01e8603a415dc8c26cf88495b8bb6ddd5a6f7bd3
Boddu Venkateswara Rao <venkateshboddu923@gmail.com>
2026-07-20 17:43:56 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -49,6 +49,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
     "http://localhost:3000",
+    "https://ecommerce-frontend-kohl-two.vercel.app",
 ]
 
 app.add_middleware(
